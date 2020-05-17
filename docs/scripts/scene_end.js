f182e926cbfc36833046f85fb9aa6cbd15ec6517
Allen Chen <achen2@mail.ccsf.edu>
2020-05-17 00:00:04 -0700

    Update scene_end.js
    Update scene_end.js

--- docs/scripts/scene_end.js
+++ docs/scripts/scene_end.js
@@ -1,8 +1,257 @@
 // JavaScript Document
 
-// DEBUG
-console.log("scene_end.js loaded successfully");
+//Start End Scene
 
-//   - Images
+//AirportBunchesOfFunTitle
+game.AirportBunchesOfFunTitleSmall = {
+	// Get handle to image
+    image: document.getElementById("AirportBunchesOfFunTitleSmall"),
+	// Declare object transform information
+    org_width: 488 * game.scale,
+    org_height: 118 * game.scale,
+    width: 0,
+    height: 0,
+    posX: 0,
+    posY: 0,
+	// Adjust the object's transform
+    resize: function () {
+        this.width = this.org_width * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+        this.height = this.org_height * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+        this.posX = 10 * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+        this.posY = 10 * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+    },
+	// Draw the object
+    draw: function () {
+        this.resize();
+        engine.context.drawImage(this.image, this.posX, this.posY, this.width, this.height);
+    }
+};
 
-//   - Buttons
+//End_Scene Play background 
+game.playBackground = {
+	// Get handle to image
+    image: document.getElementById("ABoFBackground"),
+	// Declare object transform information
+    org_width: 1920,
+    org_height: 1080,
+    width: 0,
+    height: 0,
+    posX: 0,
+    posY: 0,
+	// Adjust the object's transform
+    resize: function () {
+        this.width = engine.width;
+        this.height = engine.height;
+    },
+	// Draw the object
+    draw: function () {
+        this.resize();
+        engine.context.drawImage(this.image, this.posX, this.posY, this.width, this.height);
+    }
+};
+
+//End_Scene Game Over
+game.endGameOver = {
+	// Get handle to image
+    image: document.getElementById("endGameOver"),
+	// Declare object transform information
+    org_width: 750 * game.scale,
+    org_height: 205 * game.scale,
+    width: 0,
+    height: 0,
+    posX: 0,
+    poxY: 0,
+	// Adjust the object's transform
+    resize: function () {
+
+        this.width = this.org_width * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+        this.height = this.org_height * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+
+        this.posX = engine.width / 2 - this.width / 2;
+        this.posY = game.endGamePoints.posY / 3;
+    },
+	// Draw the object
+    draw: function () {
+        this.resize();
+        engine.context.drawImage(this.image, this.posX, this.posY, this.width, this.height);
+    }
+};
+
+//End_Scene Intials
+game.endInitials = {
+	// Get handle to image
+    image: document.getElementById("endInitials"),
+	// Declare object transform information
+    org_width: 811 * game.scale,
+    org_height: 103 * game.scale,
+    width: 0,
+    height: 0,
+    posX: 0,
+    poxY: 0,
+	// Adjust the object's transform
+    resize: function () {
+        this.width = this.org_width * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+        this.height = this.org_height * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+        this.posX = engine.width / 2 - this.width / 2;
+        this.posY = game.endKeyboardBackground.posY - (game.endKeyboardBackground.posY - (game.endGamePoints.posY + game.endGamePoints.height));
+    },
+	// Draw the object
+    draw: function () {
+        this.resize();
+        engine.context.drawImage(this.image, this.posX, this.posY, this.width, this.height);
+    }
+};
+
+//End_Scene Keypad
+game.endKeyboardBackground = {
+	// Get handle to image
+    image: document.getElementById("endKeyboardBackground"),
+	// Declare object transform information
+    org_width: 1557 * game.scale,
+    org_height: 283 * game.scale,
+    width: 0,
+    height: 0,
+    posX: 0,
+    posY: 0,
+    // Adjust the object's transform
+    resize: function () {
+        this.width = this.org_width * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+        this.height = this.org_height * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+        this.posX = engine.width / 2 - this.width / 2;
+        this.posY = engine.height - this.height;
+    },
+	// Draw the object
+    draw: function () {
+        this.resize();
+        engine.context.drawImage(this.image, this.posX, this.posY, this.width, this.height);
+    }
+};
+
+game.endKeyboardKeys = {
+	// Get handle to image
+    image: document.getElementById("endKeyboardKeys"),
+	// Declare object transform information
+    org_width: 1222 * game.scale,
+    org_height: 221 * game.scale,
+    width: 0,
+    height: 0,
+    posX: 0,
+    posY: 0,
+	// Adjust the object's transform
+    resize: function () {
+        this.width = this.org_width * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+        this.height = this.org_height * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+        this.posX = game.endKeyboardBackground.posX + 10 * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+        this.posY = game.endKeyboardBackground.posY + 10 * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+    },
+	// Draw the object
+    draw: function () {
+        this.resize();
+        engine.context.drawImage(this.image, this.posX, this.posY, this.width, this.height);
+    }
+};
+
+//End_Scene Menu Button
+game.endMenuButton = {
+	// Get handle to image
+    image: document.getElementById("wordFlightMenuButton"),
+	// Declare object transform information
+    org_width: 275 * game.scale,
+    org_height: 138 * game.scale,
+    width: 0,
+    height: 0,
+    posX: 0,
+    posY: 0,
+	// Adjust the object's transform
+    resize: function () {
+        this.width = this.org_width * (1 - engine.dimensionProportion);
+        this.height = this.org_height * (1 - engine.dimensionProportion);
+        this.posX = engine.width - this.width; // this.org_posX - engine.widthDifference;
+        this.posY = 50 * (1 - engine.dimensionProportion);
+    },
+	// Draw the object
+    draw: function () {
+        this.adjustStyle();
+    },
+	// Apply changes via CSS
+    adjustStyle: function () {
+        this.resize();
+        this.image.style.position = "absolute";
+        this.image.style.display = "block";
+        this.image.style.left = this.posX.toString() + "px";
+        this.image.style.top = this.posY.toString() + "px";
+        this.image.style.width = this.width + "px";
+        this.image.style.height = this.height + "px";
+        this.image.style.zIndex = 1;
+    }
+};
+
+//End_Scene Submit Button
+game.endSubmitButton = {
+	// Get handle to image
+    image: document.getElementById("submitButton"),
+	// Declare object transform information
+    org_width: 265 * game.scale,
+    org_height: 107 * game.scale,
+    width: 0,
+    height: 0,
+    posX: 0,
+    posY: 0,
+	// Initialize the object
+    init: function () {
+        // Add event listener to the button
+        this.image.addEventListener("click", game.endSubmitButton.clickMe);
+    },
+	// Adjust the object's transform
+    resize: function () {
+        this.width = this.org_width * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+        this.height = this.org_height * (1 - Math.max(engine.widthProportion, engine.heightProportion));
+        this.posX = game.endKeyboardBackground.posX + (game.endKeyboardBackground.width - this.width) - 10;
+        this.posY = game.endKeyboardBackground.posY + (game.endKeyboardBackground.height - this.height) / 2;
+    },
+	// Draw the object
+    draw: function () {
+        this.adjustStyle();
+    },
+	// Apply changes via CSS
+    adjustStyle: function () {
+        this.resize();
+        this.image.style.position = "absolute";
+        this.image.style.display = "block";
+        this.image.style.left = this.posX.toString() + "px";
+        this.image.style.top = this.posY.toString() + "px";
+        this.image.style.width = this.width + "px";
+        this.image.style.height = this.height + "px";
+        this.image.style.zIndex = 1;
+    },
+
+	clickMe: function () {
+        //AJAX
+        var ajax = new XMLHttpRequest();
+		// Send player's initials and score to the database
+        ajax.open("GET", "scripts/insert_score.php?u=" + game.player.initials + "&s=" + game.player.score, true);
+        ajax.send();
+
+		// Await response completion (State: 4)
+        ajax.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+				// DEBUG
+                console.log(this.responseText);
+				
+				// TRANSITION
+				// Hide keypad
+				game.inputKeypad.hideKeypad();
+				// Change game state to Leaderboard Scene
+                game.currState = game.gameState[3];
+				// Hide all elements
+                game.hideElements.hideAll();
+				// Redraw all elements
+                game.drawOnce();
+				
+				// Inform Google the player completed a playthrough
+				game.google.finish();
+            }
+        }
+    }
+};
+game.endSubmitButton.init(); // Force initialize object on first script load
